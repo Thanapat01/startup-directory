8b1fac70fc8eb044cfcffbba765ae57068c343d8
Thanapat01 <thanapat_hunter@hotmail.com>
2022-11-06 22:08:52 +0700

    add search bar

--- pages/index.js
+++ pages/index.js
@@ -48,7 +48,36 @@ export default function Home(props) {
   return (
     <div>
       <h1 className="pt-10 text-3xl text-center">Main Page</h1>
-      <div className="mt-10">
+      <div className="flex justify-center">
+        <div className="mt-10 flex justify-start w-3/4">
+          <div className="form-control">
+            <div className="input-group">
+              <input
+                type="text"
+                placeholder="Search…"
+                className="input input-bordered input-sm w-96"
+              />
+              <button className="btn btn-square btn-sm">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-4">
         <Table data={data}></Table>
       </div>
     </div>
