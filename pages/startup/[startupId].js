783467df923659ff40a0a7866365c042a2076175
Thanapat01 <thanapat_hunter@hotmail.com>
2022-11-06 20:28:47 +0700

    edit layout in startup detail page

--- pages/startup/[startupId].js
+++ pages/startup/[startupId].js
@@ -43,40 +43,50 @@ export default function StartupDetail(props) {
 
   return (
     <div className="pt-16 text-l flex justify-center">
-      <div className="grid grid-cols-4 gap-8 bg-primary-content p-4 w-3/4">
+      <div className="grid grid-cols-4 gap-8 bg-primary-content p-8 w-3/4">
         <div className="avatar row-span-2">
           <div className="w-32 rounded">
             <Image src={data.logo} layout="fill" objectFit="contain"></Image>
           </div>
         </div>
-        <div className="col-span-2 text-3xl">{data.name}</div>
-        <div className="text-end mr-16">
-          {data.origin} {data.year}
+        <div className="flex items-center">
+          <div className="badge badge-primary">name</div>
+          <div className="ml-4">{data.name}</div>
         </div>
-        <div className="col-span-3 flex items-center justify-between mr-8">
-          <div className="flex items-center">
-            <div className="badge badge-primary">Industry</div>
-            <div className="ml-2">{data.industry}</div>
-          </div>
-          <div className="flex items-center">
-            <div className="badge badge-primary">Stage</div>
-            <div className="ml-2">{data.stage}</div>
-          </div>
-          <div className="flex items-center">
-            <div className="badge badge-primary">Website</div>
-            <div>
-              <a
-                className="ml-2 link link-hover"
-                href={data.website}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {data.website}
-              </a>
-            </div>
+        <div className="flex items-center">
+          <div className="badge badge-primary">Industry</div>
+          <div className="ml-4">{data.industry}</div>
+        </div>
+        <div className="flex items-center">
+          <div className="badge badge-primary">Year</div>
+          <div className="ml-4">{data.year}</div>
+        </div>
+        <div className="flex items-center">
+          <div className="badge badge-primary">Status</div>
+          <div className="ml-4">{data.stage}</div>
+        </div>
+        <div className="col-span-2 flex items-center">
+          <div className="badge badge-primary">Origin</div>
+          <div className="ml-4">{data.origin}</div>
+        </div>
+
+        <div className="col-span-4 ml-16">
+          <div className="badge badge-primary">Description</div>
+          <div className="ml-8 indent-8">{data.about}</div>
+        </div>
+        <div className="col-start-4 flex justify-end items-center">
+          <div className="badge badge-primary">Website</div>
+          <div>
+            <a
+              className="ml-2 link link-hover"
+              href={data.website}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {data.website}
+            </a>
           </div>
         </div>
-        <div className="col-span-4 ml-16 mt-4 indent-8">{data.about}</div>
       </div>
     </div>
   );
