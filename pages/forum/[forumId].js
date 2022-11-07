95a62c9491badfa40697bf4476449b6295a34040
permcito <prutkwan@gmail.com>
2022-11-07 17:07:16 +0700

    forum table, forum item

--- pages/forum/[forumId].js
+++ pages/forum/[forumId].js
@@ -1,4 +1,14 @@
+import { useRouter } from "next/router";
 export default function ForumDetail(props) {
+  let data = props.data;
+
+  const router = useRouter();
+
+  function showDetailHandler() {
+    if (typeof data.forumId != "undefined") {
+      router.push("/pages/forum" + data.forumId);
+    }
+  }
   return (
     <div className="text-center pt-8 text-3xl">
       Forum Detail {props.forumId}
