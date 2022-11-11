e9e67c9b994be21a80e6d72ab8701a7973900f44
Thanapat01 <thanapat_hunter@hotmail.com>
2022-11-11 20:06:44 +0700

    add searchBar component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import SearchBar from "../components/searchBar";
 import Table from "../components/table";
 import styles from "../styles/Home.module.css";
 
@@ -50,31 +51,7 @@ export default function Home(props) {
       <h1 className="pt-10 text-3xl text-center">Main Page</h1>
       <div className="flex justify-center">
         <div className="mt-10 flex justify-start w-3/4">
-          <div className="form-control">
-            <div className="input-group">
-              <input
-                type="text"
-                placeholder="Search…"
-                className="input input-bordered input-sm w-96 focus:outline-none"
-              />
-              <button className="btn btn-square btn-sm btn-primary">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
+          <SearchBar></SearchBar>
         </div>
       </div>
       <div className="mt-4">
